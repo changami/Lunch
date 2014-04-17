e1e9d5f2f025fb8cf29de236d5bcf99048216495
chan_gami <babyryo@msn.com>
2014-04-18 01:57:43 +0900

    AddActivityのDB追加とMainActivityのランダムトースト実装

--- LunchDecider/src/main/java/net/changami/app/lunchdecider/AddActivity.java
+++ LunchDecider/src/main/java/net/changami/app/lunchdecider/AddActivity.java
@@ -1,25 +1,39 @@
 package net.changami.app.lunchdecider;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by chan_gami on 2014/04/17.
  */
 public class AddActivity extends Activity {
 
+    static SQLiteDatabase mydb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        Button addButton = (Button)findViewById(R.id.add_button);
+        MySQLiteOpenHelper mHelper = new MySQLiteOpenHelper(getApplicationContext());
+        mydb = mHelper.getWritableDatabase();
+
+        Button addButton = (Button) findViewById(R.id.add_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                EditText editText = (EditText) findViewById(R.id.point_name);
+                assert editText.getText() != null;
+                ContentValues values = new ContentValues();
+                values.put("name", editText.getText().toString());
+                mydb.insert("lunch_point", null, values);
+                Toast.makeText(AddActivity.this, "「" + editText.getText().toString() + "」をDBに追加しましたよ", Toast.LENGTH_LONG).show();
             }
         });
     }
--- LunchDecider/src/main/java/net/changami/app/lunchdecider/MainActivity.java
+++ LunchDecider/src/main/java/net/changami/app/lunchdecider/MainActivity.java
@@ -2,10 +2,18 @@ package net.changami.app.lunchdecider;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class MainActivity extends Activity {
@@ -19,6 +27,22 @@ public class MainActivity extends Activity {
 
         MySQLiteOpenHelper mHelper = new MySQLiteOpenHelper(getApplicationContext());
         mydb = mHelper.getReadableDatabase();
+        Cursor cursor = mydb.query("lunch_point", new String[]{"name"}, null, null, null, null, "_id DESC");
+        cursor.moveToFirst();
+        final List<String> columns = new ArrayList<String>();
+        for (boolean next = cursor.moveToFirst(); next; next = cursor.moveToNext()) {
+            columns.add(cursor.getString(0));
+        }
+
+        final Random random = new Random();
+
+        Button decideButton = (Button) findViewById(R.id.decide_button);
+        decideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, columns.get(random.nextInt(columns.size())) + "に行きましょう", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -32,7 +56,7 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_add) {
-            startActivity(new Intent());
+            startActivity(new Intent(this, AddActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
