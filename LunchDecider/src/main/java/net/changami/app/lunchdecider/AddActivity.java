1a702269308f4130fda1515080a165d0ce8531fa
chan_gami <babyryo@msn.com>
2014-07-17 01:25:15 +0900

    ButterKnifeを適用

--- LunchDecider/src/main/java/net/changami/app/lunchdecider/AddActivity.java
+++ LunchDecider/src/main/java/net/changami/app/lunchdecider/AddActivity.java
@@ -3,39 +3,39 @@ package net.changami.app.lunchdecider;
 import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import net.changami.app.lunchdecider.data.LunchPointDao;
 
-/**
- * Created by chan_gami on 2014/04/17.
- */
 public class AddActivity extends Activity {
 
     static SQLiteDatabase mydb;
     static LunchPointDao dao;
 
+    @InjectView(R.id.point_name)
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        ButterKnife.inject(this);
+
+        editText.setText("");
 
         MySQLiteOpenHelper mHelper = new MySQLiteOpenHelper(getApplicationContext());
         mydb = mHelper.getWritableDatabase();
         dao = new LunchPointDao(mydb);
 
-        Button addButton = (Button) findViewById(R.id.add_button);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText editText = (EditText) findViewById(R.id.point_name);
-                assert editText.getText() != null;
-                dao.insert(editText.getText().toString());
-                Toast.makeText(AddActivity.this, "「" + editText.getText().toString() + "」をDBに追加しましたよ", Toast.LENGTH_LONG).show();
-                editText.setText("");
-            }
-        });
+    }
+
+    @OnClick(R.id.add_button)
+    public void onClick() {
+        dao.insert(editText.getText().toString());
+        Toast.makeText(AddActivity.this, "「" + editText.getText().toString() + "」をDBに追加しました", Toast.LENGTH_LONG).show();
+        editText.setText("");
     }
 }
